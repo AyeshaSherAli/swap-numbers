9304940b1f6f0bbd0ed208f8e272204c0fab656f
AyeshaSherAli <43114900+AyeshaSherAli@users.noreply.github.com>
2020-03-13 08:19:15 +0500

    Create Swapping.py

---
+++ Swapping.py
@@ -0,0 +1,12 @@
+c=15
+d=5
+
+def swap(a,b):
+   a=a+b
+   b=a-b
+   a=a-b
+   Return a,b
+
+Print ('Before swaping c is {} and d is {}'.format(c,d))
+Print ()   
+Print ('After swapping', swap (c,d)
